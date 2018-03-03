5d61dd45461c18f4c00b7123203fdfafd34cc91b
Ben Wiedermann <bwiedermann@users.noreply.github.com>
2018-03-03 15:20:10 -0800

    A reasonable solution

--- NestedParens.java
+++ NestedParens.java
@@ -1,10 +1,25 @@
-
 public class Recursion {
-	
+
 	public static boolean nestParen(String str) {
+		// base case: the empty string
+		if (str.isEmpty()) {
+			return true;
+		}
+
+		// recursive case: if the first character is '(' and the last
+		// character is ')', then make a recursive call to a smaller version
+		// of the problem.
+		if (str.startsWith("(") && str.endsWith(")")) {
+			return nestParen(str.substring(1, str.length() - 1));
+		}
+
+		// otherwise, the string is not properly formatted
+		else {
+			return false;
+		}
 	}
 
 	public static void main(String[] args) {
-		System.out.println(nestParen("()"));
+		System.out.println(nestParen("(())"));
 	}
 }
