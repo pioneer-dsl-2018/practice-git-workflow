42c9b270b77cbeae1fdb874e98202fe4fbb1579c
Jerry-Hung <37008151+Jerry-Hung@users.noreply.github.com>
2018-03-02 19:35:13 -0800

    Update NestedParens.java

--- NestedParens.java
+++ NestedParens.java
@@ -2,6 +2,13 @@
 public class Recursion {
 	
 	public static boolean nestParen(String str) {
+		if (str.length() == 0) {
+			return true;
+		} else if (str.length() < 2) {
+			return false;
+		} 
+		return nestParen(str.substring(1, str.length() - 1));
+		
 	}
 
 	public static void main(String[] args) {
